14524ecb6d38941ecfaed3594fec06b5657c1beb
medinar <rommel.d.medina@gmail.com>
2020-03-27 10:48:39 +0800

    Spring Security Facebook Login initial commit.

---
+++ spring-security-facebook-login/src/test/java/com/medinar/springsecurityfacebooklogin/SpringSecurityFacebookLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medinar.springsecurityfacebooklogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFacebookLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
